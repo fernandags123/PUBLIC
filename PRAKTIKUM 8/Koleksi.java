61a257544ed78ce1ab4cb40efb71cbe30453899b
fernandags123 <100529240+fernandags123@users.noreply.github.com>
2025-05-11 18:16:49 +0700

    Create Koleksi.java

---
+++ PRAKTIKUM 8/Koleksi.java
@@ -0,0 +1,14 @@
+public abstract class Anabul {
+    protected String nama;
+
+    public Anabul(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public abstract String bunyi();
+    public abstract String gerak();
+}
